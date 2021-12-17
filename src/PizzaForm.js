1f2328608c05ed454a0a38753212fc4c702be685
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-12-17 14:23:52 -0600

    starts looking like a website, a form added, 5/9 tests passing!

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -5,11 +5,13 @@ import axios from 'axios';
 import * as yup from 'yup';
 
 const initialFormValues = {
-    name: '',
-    size: '',
-    topping1: false,
+    name: '',   /// text input
+    size: '',   ///dropdown
+    topping1: false,  //checkboxes below
     topping2: false,
-    special: ''
+    topping3: false,
+    topping4: false,
+    special: ''    //text input with spec instructions
 }
 
 
@@ -17,7 +19,13 @@ const initialFormValues = {
 
 function PizzaForm(props){
 
-  const{values,submit} = props
+  const{values, update, submit} = props
+
+  const onChange = evt => {
+    const name = evt.target.name;
+    const { value } = evt.target;
+    update(name, value);
+  }
 
   const onSubmit = evt => {
     evt.preventDefault()
@@ -26,12 +34,84 @@ function PizzaForm(props){
 
   return(
     <form className = 'form container' onSubmit={onSubmit}>
-      <div className='form submit'>
-        <h2>Build your pizza!</h2>
-        <button>SUBMIT ORDER</button>
+      <div className='form-group inputs'>
         <button> 
             <Link to={`/`}>Home</Link>
         </button>
+      <h1>Build your pizza!</h1>
+
+        <label>Name
+              <input id='name-input'
+                name="name"
+                type="text"
+                placeholder="Type your name"
+                maxLength="15"
+                // value={values.name}
+                onChange={onChange}
+              />
+        </label>
+
+
+        <label>Size
+              <select id="size-dropdown" onChange={onChange}>
+                <option value="">-- Select a Size --</option>
+                <option value="S">Small</option>
+                <option value="M">Medium</option>
+                <option value="L">Large</option>
+                <option value="XL">ExtraLarge</option>
+              </select>
+        </label>
+
+        <h2>Choose your toppings!!!</h2>
+
+        <label>Topping1
+          <input
+            onChange={onChange}
+            name='topping1'
+            type='checkbox'
+          />
+        </label>
+
+        <label>Topping2
+          <input
+            onChange={onChange}
+            name='topping2'
+            type='checkbox'
+          />
+        </label>
+
+        <label>Topping3
+          <input
+            onChange={onChange}
+            name='topping3'
+            type='checkbox'
+          />
+        </label>
+
+        <label>Topping4
+          <input
+            onChange={onChange}
+            name='topping4'
+            type='checkbox'
+          />
+        </label>
+
+        <label>Special Instructions
+              <input id='special-text'
+                name="special"
+                type="text"
+                placeholder="Anything else you want to add?"
+                maxLength="100"
+                // value={values.name}
+                onChange={onChange}
+              />
+        </label>
+
+
+
+      <div className='form submit'>
+        <button>SUBMIT ORDER</button>
+      </div>
       </div>
     </form>
   )
